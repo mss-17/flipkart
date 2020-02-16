638bfeabd8e2635de31f7a622079f95acb0f0324
madhusudhana <madhu1952@gmail.com>
2020-02-16 22:12:01 +0530

    Revert "updated file"
    This reverts commit 64e28a1f7955750ecaa52c6382529221eeeaa705.

--- madhu.java
+++ madhu.java
@@ -1,4 +1,4 @@
 today class is github
 what is github repository...?
-  ANSWER:  git hub repository is a just file location
- where you are storing all files related in project
+   git hub repository is a just file location
+where you are storing all files related in project
